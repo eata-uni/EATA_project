7c58dd6ce17337458e8b9ee219dfc0ca17b9c211
luischanquettih <99174520+luischanquettih@users.noreply.github.com>
2023-06-09 17:03:50 -0500

    Delete Entregable1 directory

--- Entregable1/packages/auth.py
+++
@@ -1,7 +0,0 @@
-from pydrive2.auth import GoogleAuth
-
-def start():
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
-    gauth.SaveCredentialsFile('credentials_module.json')
-
--- Entregable1/packages/id.py
+++
@@ -1,5 +0,0 @@
-def Luis():
-    return 5175931855
-
-def Elmer():
-    return 1516058597
